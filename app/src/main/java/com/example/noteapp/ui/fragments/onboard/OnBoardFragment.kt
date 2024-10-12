28bf144723d3c36b4a7326341e0bf7931c9cd78f
kaasym1 <kasymmuhammaduulu@gmail.com>
2024-10-12 18:32:06 +0600

    hw3

--- app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardFragment.kt
@@ -5,31 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentOnBoardBinding
-import com.example.noteapp.databinding.FragmentOnBoardPagingBinding
 import com.example.noteapp.ui.adapters.OnBoardPagerAdapter
+import com.example.noteapp.utils.PreferenceHelper
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+
 
 class OnBoardFragment : Fragment() {
 
     private lateinit var binding: FragmentOnBoardBinding
+    private lateinit var sharedPreferences: PreferenceHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentOnBoardBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initialize()
-        setupListener()
-        dotsIndicator()
+        sharedPreferences = PreferenceHelper()
+        sharedPreferences.init(requireContext())
+
+        if (sharedPreferences.isOnBoardingCompleted()) {
+            findNavController().navigate(R.id.action_onBoardFragment_to_noteFragment)
+        } else {
+            initialize()
+            setupListener()
+            dotsIndicator()
+        }
     }
 
     private fun initialize() {
@@ -42,22 +52,26 @@ class OnBoardFragment : Fragment() {
                 super.onPageSelected(position)
                 if (position == 2) {
                     binding.btnStart.visibility = View.VISIBLE
-                    binding.txtSkip.visibility = View.INVISIBLE
+                    binding.txtSkip.visibility = View.GONE
                 } else {
-                    binding.btnStart.visibility = View.INVISIBLE
+                    binding.btnStart.visibility = View.GONE
                     binding.txtSkip.visibility = View.VISIBLE
+                    binding.txtSkip.setOnClickListener {
+                        if (currentItem < 3) {
+                            setCurrentItem(currentItem + 2, true)
+                        }
+                    }
                 }
             }
         })
-        binding.txtSkip.setOnClickListener {
-            if (currentItem < 2) {
-                setCurrentItem(currentItem + 2, true)
-            }
+        binding.btnStart.setOnClickListener {
+            sharedPreferences.setOnBoardingCompleted(true)
+            findNavController().navigate(R.id.action_onBoardFragment_to_noteFragment)
         }
     }
+
     private fun dotsIndicator() = with(binding.viewpager2) {
-        TabLayoutMediator(binding.tabLayout, binding.viewpager2) { tab, position ->
-        }.attach()
+        TabLayoutMediator(binding.tabLayout, binding.viewpager2) { tab, position -> }.attach()
         registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
@@ -66,6 +80,7 @@ class OnBoardFragment : Fragment() {
         })
         updateDotsIndicator(binding.tabLayout, 0)
     }
+
     private fun updateDotsIndicator(tabLayout: TabLayout, selectedPosition: Int) {
         for (i in 0 until tabLayout.tabCount) {
             val tab = tabLayout.getTabAt(i)
